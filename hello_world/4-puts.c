db75421a704914829440c9148a080e7a14b60f00
spcruz5 <spcruz5@gmail.com>
2022-06-06 18:10:26 +0000

    C program that prints

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -2,9 +2,6 @@
 #include<stdio.h>
 int main(void)
 
-{
-	char line[] = "\"Programming is like building a multilingual puzzle\n";
-
-	puts(line);
+{	puts("Programming is like building a multilingual puzzle\n");
 	return (0);
 }
