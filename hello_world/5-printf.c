05d54ffb2661d46690aad4ef1fdea0ace92ff32e
spcruz5 <spcruz5@gmail.com>
2022-06-06 18:46:09 +0000

    C program that prints using printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,13 @@
+/*C program that prints using printf*/
+#include <stdio.h>
+
+/**
+  * main - Prints a text
+  *
+  * Return: 0
+  */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
