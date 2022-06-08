f4db39d963c9eace7e2996c451797006606cae36
spcruz5 <spcruz5@gmail.com>
2022-06-08 17:35:34 +0000

    C program that prints 1-9 putchar

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - program
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+return (0);
+}
