9587435d42ba5a71e0c710aed4a3cfe59bf1f2a1
spcruz5 <spcruz5@gmail.com>
2022-06-08 16:01:29 +0000

    Program that prints lower case alphabet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main - Program that print lower case letter
+ *
+ *Return: 0
+ */
+int main(void)
+{
+for (int n = 'a'; n <= 'z'; n++)
+{
+	putchar (n);
+	putchar ('\n');
+}
+return (0);
+}
