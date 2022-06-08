3987862229cc4e7381550fca8779b02e09b30420
spcruz5 <spcruz5@gmail.com>
2022-06-08 17:19:35 +0000

    C program that prints 0-9

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Program that prints
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+return (0);
+}
