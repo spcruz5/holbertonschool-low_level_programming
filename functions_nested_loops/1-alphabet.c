f2b7980285058effb6eddef484ceaf80d18fae3a
spcruz5 <spcruz5@gmail.com>
2022-06-09 04:12:48 +0000

    Program that print alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  * print_alphabet - alphabet
+  *
+  * Return: 0
+  */
+
+void print_alphabet(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		_putchar(x);
+	}
+
+	_putchar('\n');
+}
