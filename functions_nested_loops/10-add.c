9877bfe4199c97d9e3dfa4997f3a9f624df6d445
spcruz5 <spcruz5@gmail.com>
2022-06-09 21:40:25 +0000

    Function that adds 2int

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - adds two integers and returns result
+ * @x: first input
+ * @y: second input
+ * Return: one + two
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
