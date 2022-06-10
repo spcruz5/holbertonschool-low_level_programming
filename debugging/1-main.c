01e7ddc38abb9f4310223a527034c21102612e17
spcruz5 <spcruz5@gmail.com>
2022-06-10 02:49:22 +0000

    Comment how to fix bug

--- debugging/1-main.c
+++ debugging/1-main.c
@@ -7,16 +7,16 @@
 
 int main(void)
 {
-        int i;
-	
+	int i;
+
 	printf("Infinite loop incoming :(\n");
 
 	i = 0;
 
 	while (i < 10)
-        {
-		putchar(i);	//needs an increment i++ so that 
-	}			//the i=10 and the loop can finish
+	{
+		putchar(i); /*needs an increment i++ so that*/
+	}		/*the i=10 and the loop can finish*/
 
 	printf("Infinite loop avoided! \\o/\n");
 
