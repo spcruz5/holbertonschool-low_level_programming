bb8b333d3f26b1226392dbb76578fc7b22224169
spcruz5 <spcruz5@gmail.com>
2022-06-13 23:40:17 +0000

    Function that returns 1 if is 1-9

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *_isdigit - checks for a digit (0-9)
+ *@c: The number to be checked
+ *
+ *Return: 1 if number is a digit, else 0
+ */
+
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+
+	else
+		return (0);
+}
