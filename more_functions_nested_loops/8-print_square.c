c9a7ac836d7f1a9af199246c93d44d0ecc21c495
spcruz5 <spcruz5@gmail.com>
2022-06-14 01:06:52 +0000

    Function that prints #char

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_square - Prints #.
+ * @size: The size of the square.
+ */
+
+void print_square(int size)
+{
+	int x, y;
+
+	if (size > 0)
+	{
+		for (x = 0; x < size; x++)
+		{
+			for (y = 0; y < size; y++)
+				_putchar('#');
+
+			if (x == size - 1)
+				continue;
+			_putchar('\n');
+		}
+	}
+
+	_putchar('\n');
+}
