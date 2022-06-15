92281ddcadc10cc01863e21ebdc7740fb8ace65d
spcruz5 <spcruz5@gmail.com>
2022-06-15 23:58:50 +0000

    Function that copares two strings

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+  * _strcmp - compares two strings
+  * @s1: string 1
+  * @s2: string 2
+  * Return: result
+  */
+
+int _strcmp(char *s1, char *s2)
+{
+	char result = 0;
+
+	while (*s1 != '\0' && *s2 != '\0' && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	result = *s1 - *s2;
+	return (result);
+}
