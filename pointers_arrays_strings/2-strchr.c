b5ef7c5b7e7caaffc57b5bb434521fd5a73a6a64
spcruz5 <spcruz5@gmail.com>
2022-06-17 13:24:03 +0000

    Function that locates a char in a string

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+*  *_strchr - fun locates a char in string
+* @s: string
+* @c: char
+* Return: char
+*/
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	i = 0;
+	while (s[i] >= '\0')
+	{
+		if (s[i] == c)
+		{
+		return (&s[i]);
+		}
+	i++;
+	}
+}
