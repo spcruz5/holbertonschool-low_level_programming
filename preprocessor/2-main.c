85c4460cac929d0fd0537bd3bfa485df72fe8d4b
spcruz5 <spcruz5@gmail.com>
2022-06-29 02:59:38 +0000

    Function that print file name

--- preprocessor/2-main.c
+++ preprocessor/2-main.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-/*
-*main- function that prints file name
+/**
+*main - function that prints file name
 *Return: 0
 */
 
 int main(void)
 {
-	printf("%s\n",__FILE__);
+	printf("%s\n", __FILE__);
 	return (0);
 }
