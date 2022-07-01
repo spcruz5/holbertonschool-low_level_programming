c365275b1b8432c401c03792f7a00ad229206fcc
spcruz5 <spcruz5@gmail.com>
2022-07-01 05:07:20 +0000

    Header file dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,5 +1,5 @@
 #ifndef _DOG_H_
-#define _DOG_H_
+#define _DOG_H_ 
 /**
  * struct dog - Define a struct dog
  * @name: name
@@ -12,16 +12,15 @@ struct dog
 	char *name;
 	float age;
 	char *owner;
-};
+} dog;
 
-typedef struct dog dog_t;
+typedef dog dog_t;
 
+/** PROTOTYPES */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-
-int _putchar(char c);
 
 #endif
