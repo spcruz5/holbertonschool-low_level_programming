55ceeaea88e13c49a0be55905c662e20473f33a1
spcruz5 <spcruz5@gmail.com>
2022-07-05 14:59:28 +0000

    All 3

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+* op_add - sum
+* @a: int
+* @b: int
+* Return: result.
+*/
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+* op_sub - resta
+* @a: int
+* @b: int
+* Return: result
+*/
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+* op_mul - multi
+* @a: int
+* @b: int
+* Return: result
+**/
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+* op_div - divi
+* @a: int
+* @b: int
+* Return: result
+*/
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+* op_mod - mod
+* @a: int
+* @b: int
+* Return: result
+*/
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
