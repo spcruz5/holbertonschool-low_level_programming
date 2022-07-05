0f6c4ab9b12ecf45478a9c5670b9d33dbc145d47
spcruz5 <spcruz5@gmail.com>
2022-07-05 14:51:42 +0000

    Function that prints num

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include "variadic_functions.h"
+/**
+ * print_numbers - fun that prints num
+ * @separator: char
+ * @n: integer n
+ * Return: 0
+*/
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int a;
+	va_list list;
+
+	va_start(list, n);
+	for (a = 0; a < n; a++)
+	{
+		printf("%d", va_arg(list, int));
+
+		if (a != (n - 1) && separator != NULL)
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+	va_end(list);
+}
