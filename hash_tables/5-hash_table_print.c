78e993c5e9e629eb7435b85dddfb3bcde538ace0
spcruz5 <spcruz5@gmail.com>
2022-08-25 22:48:28 +0000

    Function that prints a hash function

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -1,31 +1,30 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_print - prints keys and vals of the hash table
- *
- * @ht: pointer to the hash table
- * Return: no return
+ * hash_table_print - prints a hash table
+ * @ht: hash table
+ * Return: Void
  */
 void hash_table_print(const hash_table_t *ht)
 {
-	unsigned long int i;
-	hash_node_t *tmp;
-	char *sep;
+	unsigned int i;
+	hash_node_t *tmp = NULL;
+	char *d = "";
 
 	if (ht == NULL)
 		return;
-
 	printf("{");
-	sep = "";
-
 	for (i = 0; i < ht->size; i++)
 	{
-		tmp = ht->array[i];
-		while (tmp != NULL)
+		if (ht->array[i] != NULL)
 		{
-			printf("%s'%s': '%s'", sep, tmp->key, tmp->value);
-			sep = ", ";
-			tmp = tmp->next;
+			tmp = ht->array[i];
+			while (tmp != NULL)
+			{
+				printf("%s\'%s\': \'%s\'", d, tmp->key, tmp->value);
+				d = ", ";
+				tmp = tmp->next;
+			}
 		}
 	}
 	printf("}\n");
